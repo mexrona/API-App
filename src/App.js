4b13c1383704234cc8902949b0fd5531aa9f350a
mexrona <tuichievamexrona@gmail.com>
2024-11-14 15:28:51 +0300

    API-App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [posts, setPosts] = useState([]);
+
+    const getPosts = async () => {
+        const response = await fetch(
+            "https://jsonplaceholder.typicode.com/posts"
+        );
+
+        const posts = await response.json();
+
+        const changedPosts = posts.map((post) => post.title);
+
+        setPosts(changedPosts);
+    };
+
+    const getPost = async () => {
+        const response = await fetch(
+            "https://jsonplaceholder.typicode.com/posts"
+        );
+
+        const postsNeed = await response.json();
+
+        const changedPost = postsNeed.map((postNeed) => {
+            const inputValue = document.getElementsByClassName("Id")[0].value;
+
+            if (postNeed.id === Number(inputValue)) {
+                return postNeed.body;
+            }
+        });
+
+        setPosts(changedPost);
+    };
+
+    return (
+        <div className="App">
+            <br />
+            <input className="Id" placeholder="ID" />
+            <button onClick={getPost}>Получить конкретный пост</button>
+            <br />
+            <br />
+            <div>Список постов:</div>
+            <div>
+                {posts.map((post, index) => (
+                    <div key={index}>
+                        <br />
+                        {post}
+                    </div>
+                ))}
+            </div>
+            <br />
+            <button onClick={getPosts}>Получить посты</button>
+            <br />
+            <br />
+        </div>
+    );
 }
 
 export default App;
